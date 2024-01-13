709364fb4b6faa2e64d7955a59a10b1e57d320d1
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-01-13 11:52:16 -0500

    add code back

---
+++ employees-back/src/main/java/com/employees/employeesback/EmployeesBackApplication.java
@@ -0,0 +1,13 @@
+package com.employees.employeesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesBackApplication.class, args);
+	}
+
+}
